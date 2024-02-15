d9061c5aa967c158d5fb60005576464f7e40f6f2
Lorena Silva <lorenagrazy@yahoo.com.br>
2024-02-15 14:59:30 -0300

    Update

--- LABFashion/src/app/app-routing.module.ts
+++ LABFashion/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 import { LoginComponent } from './components/pages/login/login.component';
 import { ForgotpasswordComponent } from './components/pages/forgotpassword/forgotpassword.component';
 import { SentLinkComponent } from './components/pages/sent-link/sent-link.component';
@@ -16,80 +15,27 @@ import { TemplateEditingComponent } from './components/pages/template-editing/te
 import { FullComponent } from './components/hall/full/full.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'userregistration', component: UserregistrationComponent },
+  { path: 'forgotpassword', component: ForgotpasswordComponent },
+  { path: 'forgotpassword/sent-link', component: SentLinkComponent },
   {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'userregistration',
-    component: UserregistrationComponent
-  },
-  {
-    path: 'forgotpassword',
-    component: ForgotpasswordComponent
-  },
-  {
-    path: 'forgotpassword/sent-link',
-    component: SentLinkComponent
-  },
-  {
-    path: '',
+    path: 'menu/anchor',
     component: FullComponent,
     children: [
-      {
-        path: 'menu/anchor/dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'menu/anchor/collection-listing',
-        component: CollectionListingComponent
-      },
-      {
-        path: 'menu/anchor/collection-registration',
-        component: CollectionRegistrationComponent
-      },
-      {
-        path: 'menu/anchor/collection-edition',
-        component: CollectionEditionComponent
-      },
-
-      {
-        path: 'menu/anchor/collection-edition/:id',
-        component: CollectionEditionComponent
-      },
-
-      {
-        path: 'menu/anchor/template-listing',
-        component: TemplateListingComponent
-      },
-      {
-        path: 'menu/anchor/template-editing/:id',
-        component: TemplateEditingComponent
-      },
-
-      {
-        path: 'menu/anchor/model-registration',
-        component: ModelRegistrationComponent
-      },
-      {
-        path: 'menu/anchor/template-editing',
-        component: TemplateEditingComponent
-      },]
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'collection-listing', component: CollectionListingComponent },
+      { path: 'collection-registration', component: CollectionRegistrationComponent },
+      { path: 'collection-edition', component: CollectionEditionComponent },
+      { path: 'collection-edition/:id', component: CollectionEditionComponent },
+      { path: 'template-listing', component: TemplateListingComponent },
+      { path: 'template-editing/:id', component: TemplateEditingComponent },
+      { path: 'model-registration', component: ModelRegistrationComponent },
+      { path: 'template-editing', component: TemplateEditingComponent }
+    ]
   },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
-
-
-
-
-
-
-
-
-
-
-
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
